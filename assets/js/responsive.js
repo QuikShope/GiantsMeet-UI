2456d5ad88bcc3db5683d1de542b378e4c695637
Tanmoy741127 <ts741127@gmail.com>
2021-04-15 00:46:21 +0530

    Responsive Update #11

--- assets/js/responsive.js
+++
@@ -1,8 +0,0 @@
-const resizeElementsAuto = ()=>{
-    document.getElementById("mainBody").style.height=`${window.screen.height-document.querySelector(".logo").clientHeight}px`;
-    console.log("Trigerred");
-}
-
-resizeElementsAuto();
-
-window.onresize = resizeElementsAuto;
